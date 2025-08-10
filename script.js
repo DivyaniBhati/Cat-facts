52c029f04f50e61ae0d1c2eb796be5a6ab3dd2d7
Divyani Bhati <divyanibhati78@gmail.com>
2025-08-10 11:01:23 +0530

    facts API added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-var card = document.getElementsByClassName(".card");
+let facts = document.querySelectorAll(".facts");
+
+facts.forEach((factCard) => {
+  factCard.addEventListener("mouseenter", async function () {
+    do {
+      fact = await getfacts();
+    } while (fact.length > 170);
+
+    console.log(fact.length);
+    let p = document.querySelectorAll(".card-back p");
+    p.forEach((data) => {
+      data.innerText = fact;
+    });
+  });
+});
+
+let url = "https://catfact.ninja/fact";
+
+async function getfacts() {
+  try {
+    let res = await axios.get(url);
+    return res.data.fact;
+  } catch (e) {
+    console.log("ERROR - ", e);
+    return "NO FACT FOUND";
+  }
+}
